476d12510962b75fac9b76d205ce0ea84d005b19
Takahiro <hogehoge@gachapin.jp>
2018-10-28 05:17:43 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-emcc -O2 -o ./WebGLRenderer.js ./c/*.c --memory-init-file 0 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s EXPORTED_FUNCTIONS="['_sizeOfObject3D', '_Object3D_init', '_sizeOfWebGLRenderer', '_WebGLRenderer_init', '_WebGLRenderer_setSize', '_WebGLRenderer_clearColor', '_WebGLRenderer_render']" -s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap', 'stringToUTF8']"
+emcc -O2 -o ./three-wasm.js ./cpp/*.cpp ./cpp/*/*.cpp --memory-init-file 0 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s EXPORTED_FUNCTIONS="['_sizeOfObject3D', '_Object3D_init', '_Object3D_add', '_sizeOfPerspectiveCamera', '_PerspectiveCamera_init', '_sizeOfWebGLRenderer', '_WebGLRenderer_init', '_WebGLRenderer_setSize', '_WebGLRenderer_clearColor', '_WebGLRenderer_render']" -s EXTRA_EXPORTED_RUNTIME_METHODS="['ccall', 'cwrap', 'stringToUTF8']"
