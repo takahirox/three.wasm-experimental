e123faeb4403b2511f8c560c931c2ba3c63b6eb2
Takahiro <hogehoge@gachapin.jp>
2018-10-28 05:17:26 +0900

    Update export.cpp

--- cpp/export.cpp
+++ cpp/export.cpp
@@ -1,9 +1,14 @@
 ﻿#include "./core/Object3D.h"
+#include "./cameras/PerspectiveCamera.h"
 #include "./renderer/WebGLRenderer.h"
 
 extern "C" {
 	int sizeOfObject3D() {
 		return sizeof(Object3D);
+	}
+
+	int sizeOfPerspectiveCamera() {
+		return sizeof(PerspectiveCamera);
 	}
 
 	int sizeOfWebGLRenderer() {
@@ -14,6 +19,23 @@ extern "C" {
 		Object3D *object
 	) {
 		return new(object) Object3D();
+	}
+
+	Object3D* Object3D_add(
+		Object3D *object,
+		Object3D *child
+	) {
+		return object->add(child);
+	}
+
+	PerspectiveCamera* PerspectiveCamera_init(
+		PerspectiveCamera* camera,
+		double fov,
+		double aspect,
+		double near,
+		double far
+	) {
+		return new(camera) PerspectiveCamera(fov, aspect, near, far);
 	}
 
 	WebGLRenderer* WebGLRenderer_init(
@@ -43,8 +65,9 @@ extern "C" {
 
 	WebGLRenderer* WebGLRenderer_render(
 		WebGLRenderer *renderer,
-		Object3D *scene
+		Object3D *scene,
+		Camera *camera
 	) {
-		return renderer->render(scene);
+		return renderer->render(scene, camera);
 	}
 }
