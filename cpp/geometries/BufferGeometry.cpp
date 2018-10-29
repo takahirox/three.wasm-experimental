f465c4550cedea638aa3f43170078b80aefd2407
Takahiro <hogehoge@gachapin.jp>
2018-10-30 04:10:04 +0900

    Add Geometries

---
+++ cpp/geometries/BufferGeometry.cpp
@@ -0,0 +1,29 @@
+﻿#include "BufferGeometry.h"
+
+BufferGeometry::BufferGeometry()
+: index(NULL) {}
+
+BufferAttribute* BufferGeometry::getIndex() {
+	return this->index;
+}
+
+BufferGeometry* BufferGeometry::setIndex(
+	BufferAttribute *_index
+) {
+	this->index = _index;
+	return this;
+}
+
+BufferGeometry* BufferGeometry::addAttribute(
+	std::string name,
+	BufferAttribute *attribute
+) {
+	this->attributes[name] = attribute;
+	return this;
+}
+
+BufferAttribute* BufferGeometry::getAttribute(
+	std::string name
+) {
+	return this->attributes[name];
+}
---
+++ cpp/geometries/BufferGeometry.h
@@ -0,0 +1,31 @@
+﻿#ifndef __THREE_BUFFER_GEOMETRY__
+#define __THREE_BUFFER_GEOMETRY__
+
+#include <map>
+#include <string>
+#include "../core/BufferAttribute.h"
+
+class BufferGeometry {
+public:
+	BufferAttribute *index;
+	std::map<std::string, BufferAttribute*> attributes;
+
+	BufferGeometry();
+
+	BufferAttribute* getIndex();
+
+	BufferGeometry* setIndex(
+		BufferAttribute *_index
+	);
+
+	BufferGeometry* addAttribute(
+		std::string name,
+		BufferAttribute *attribute
+	);
+
+	BufferAttribute* getAttribute(
+		std::string name
+	);
+};
+
+#endif
