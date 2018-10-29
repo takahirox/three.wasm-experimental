5466d4f4d89df7141e561f4f31e4950c09c8dd04
Takahiro <hogehoge@gachapin.jp>
2018-10-30 04:09:40 +0900

    Add BufferAttribute

---
+++ cpp/core/BufferAttribute.cpp
@@ -0,0 +1,18 @@
+﻿#include "BufferAttribute.h"
+
+BufferAttribute::BufferAttribute(
+	void *_array,
+	type _dataType,
+	int _dataLength,
+	int _itemSize
+): array(_array), dataType(_dataType), itemSize(_itemSize),
+	needsUpdate(false), version(0) {
+	if(_dataType == Float32) {
+		this->bytePerElement = 4;
+	} else if(_dataType == Uint16) {
+		this->bytePerElement = 2;
+	} else {
+		this->bytePerElement = 1;
+	}
+	this->count = _dataLength / this->itemSize / this->bytePerElement;
+}
---
+++ cpp/core/BufferAttribute.h
@@ -0,0 +1,23 @@
+﻿#ifndef __THREE_BUFFER_ATTRIBUTE__
+#define __THREE_BUFFER_ATTRIBUTE__
+
+class BufferAttribute {
+public:
+	void* array;
+	enum type { Float32, Uint16, Uint8 };
+	type dataType;
+	int itemSize;
+	int count;
+	int bytePerElement;
+	int version;
+	bool needsUpdate;
+
+	BufferAttribute(
+		void *_array,
+		type _dataType,
+		int _dataLength,
+		int _itemSize
+	);
+};
+
+#endif
