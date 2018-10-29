8147ba5b80ec46a8ee35ca984c05c22f895838d9
Takahiro <hogehoge@gachapin.jp>
2018-10-30 04:12:49 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,17 +3,26 @@
 emcc \
 	-O2\
 	-o ./three-wasm.js\
-	./cpp/*.cpp ./cpp/*/*.cpp \
+	./cpp/*.cpp ./cpp/*/*.cpp ./cpp/*/*/*.cpp \
 	--memory-init-file 0\
 	-s WASM=1\
 	-s ALLOW_MEMORY_GROWTH=1\
 	-s EXPORTED_FUNCTIONS="[\
+	'_sizeOfBufferAttribute',\
+	'_sizeOfBufferGeometry',\
 	'_sizeOfObject3D',\
+	'_sizeOfMesh',\
 	'_sizeOfScene',\
 	'_sizeOfPerspectiveCamera',\
 	'_sizeOfWebGLRenderer',\
+	'_BufferAttribute_init',\
+	'_BufferGeometry_init',\
+	'_BufferGeometry_setIndex',\
+	'_BufferGeometry_addAttribute',\
 	'_Object3D_init',\
 	'_Object3D_add',\
+	'_Object3D_getPositionPointer',\
+	'_Mesh_init',\
 	'_Scene_init',\
 	'_PerspectiveCamera_init',\
 	'_WebGLRenderer_init',\
