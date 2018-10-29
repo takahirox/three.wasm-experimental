a4ca9e4f72672a99cca142009ac75b953e8b2171
Takahiro <hogehoge@gachapin.jp>
2018-10-30 06:32:22 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,13 +8,16 @@ emcc \
 	-s WASM=1\
 	-s ALLOW_MEMORY_GROWTH=1\
 	-s EXPORTED_FUNCTIONS="[\
+	'_sizeOfVector3',\
 	'_sizeOfBufferAttribute',\
 	'_sizeOfBufferGeometry',\
 	'_sizeOfObject3D',\
 	'_sizeOfMesh',\
+	'_sizeOfMeshBasicMaterial',\
 	'_sizeOfScene',\
 	'_sizeOfPerspectiveCamera',\
 	'_sizeOfWebGLRenderer',\
+	'_Vector3_init',\
 	'_BufferAttribute_init',\
 	'_BufferGeometry_init',\
 	'_BufferGeometry_setIndex',\
@@ -23,6 +26,7 @@ emcc \
 	'_Object3D_add',\
 	'_Object3D_getPositionPointer',\
 	'_Mesh_init',\
+	'_MeshBasicMaterial_init',\
 	'_Scene_init',\
 	'_PerspectiveCamera_init',\
 	'_WebGLRenderer_init',\
