d9cb1a9c1f36fb8c2a75206492fb37373b3dbcdb
Takahiro <hogehoge@gachapin.jp>
2018-11-01 23:10:54 +0900

    Add Sphere

---
+++ cpp/math/Sphere.cpp
@@ -0,0 +1,20 @@
+﻿#include "Sphere.h"
+
+Sphere::Sphere() : radius(0.0) {
+}
+
+Sphere* Sphere::copy(
+	Sphere *sphere
+) {
+	this->center.copy(&sphere->center);
+	this->radius = sphere->radius;
+	return this;
+}
+
+Sphere* Sphere::applyMatrix4(
+	Matrix4 *matrix
+) {
+	this->center.applyMatrix4(matrix);
+	this->radius = this->radius * matrix->getMaxScaleOnAxis();
+	return this;
+}
---
+++ cpp/math/Sphere.h
@@ -0,0 +1,23 @@
+﻿#ifndef __THREE_SPHERE__
+#define __THREE_SPHERE__
+
+#include "Vector3.h"
+#include "Matrix4.h"
+
+class Sphere {
+public:
+	Vector3 center;
+	double radius;
+
+	Sphere();
+
+	Sphere* copy(
+		Sphere *sphere
+	);
+
+	Sphere* applyMatrix4(
+		Matrix4 *matrix
+	);
+};
+
+#endif
