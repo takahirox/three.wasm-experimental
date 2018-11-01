0423d74a06d6f123d95f1a399451c8d2fc7ef201
Takahiro <hogehoge@gachapin.jp>
2018-11-01 23:10:41 +0900

    Add Plane

---
+++ cpp/math/Plane.cpp
@@ -0,0 +1,29 @@
+﻿#include "Plane.h"
+
+Plane::Plane() : constant(0.0) {
+	this->normal.set(1.0, 0.0, 0.0);
+}
+
+Plane* Plane::setComponents(
+	double x,
+	double y,
+	double z,
+	double w
+) {
+	this->normal.set(x, y, z);
+	this->constant = w;
+	return this;
+}
+
+Plane *Plane::normalize() {
+	double inverseNormalLength = 1.0 / this->normal.length();
+	this->normal.multiplyScalar( inverseNormalLength );
+	this->constant *= inverseNormalLength;
+	return this;
+}
+
+double Plane::distanceToPoint(
+	Vector3 *point
+) {
+	return this->normal.dot(point) + this->constant;
+}
---
+++ cpp/math/Plane.h
@@ -0,0 +1,27 @@
+﻿#ifndef __THREE_PLANE__
+#define __THREE_PLANE__
+
+#include "Vector3.h"
+
+class Plane {
+public:
+	Vector3 normal;
+	double constant;
+
+	Plane();
+
+	Plane* setComponents(
+		double x,
+		double y,
+		double z,
+		double w
+	);
+
+	Plane* normalize();
+
+	double distanceToPoint(
+		Vector3 *point
+	);
+};
+
+#endif
